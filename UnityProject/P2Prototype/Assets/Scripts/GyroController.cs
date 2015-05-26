ed42020b2eb00b9823cbad2b1faa4e030981c145
Mporsi <Atrixia@gmail.com>
2015-05-26 11:03:28 +0200

    Revert "Revert "Cleaned up some code""
    This reverts commit 19388a96ad3f7e22727bade06196e1a107de91dc.
    
    Conflicts:
    	UnityProject/P2Prototype/P2Prototype-csharp.sln
    	UnityProject/P2Prototype/P2Prototype.sln

--- UnityProject/P2Prototype/Assets/Scripts/GyroController.cs
+++ UnityProject/P2Prototype/Assets/Scripts/GyroController.cs
@@ -3,99 +3,53 @@
 
 public class GyroController : MonoBehaviour {
 
-    private bool gyroEnabled = true;
-    private const float lowPassFilterFactor = 0.1f;
+    private const float lowPassFilter = 0.1f;
 
     private Quaternion baseIdentity = Quaternion.Euler(90, 0, 0);
 
     private Quaternion cameraBase = Quaternion.identity;
     private Quaternion calibration = Quaternion.identity;
     private Quaternion baseOrientation = Quaternion.Euler(90, 0, 0);
-    private Quaternion baseOrientationRotationFix = Quaternion.identity;
-
     private Quaternion referanceRotation = Quaternion.identity;
 
 
 
-    protected void Start() {
-        AttachGyro();
+    void Start() {
+        ResetBaseOrientation();
+        UpdateBaseRotation();
+        RecalculateReferenceRotation();
         Input.gyro.enabled = true;
     }
 
-    protected void Update() {
-        if (!gyroEnabled)
-            return;
+    void Update() {
         transform.rotation = Quaternion.Slerp(transform.rotation,
-                                              cameraBase * (ConvertRotation(referanceRotation * Input.gyro.attitude) * GetRotFix()),
-                                              lowPassFilterFactor);
-        Debug.Log(Input.gyro.attitude);
-    }
-
-
-    // Attaches gyro controller to the transform.
-    private void AttachGyro() {
-        gyroEnabled = true;
-        ResetBaseOrientation();
-        UpdateCalibration(true);
-        UpdateCameraBaseRotation(true);
-        RecalculateReferenceRotation();
-    }
-
-
-    // Detaches gyro controller from the transform
-    private void DetachGyro() {
-        gyroEnabled = false;
-    }
-
-
-    // Update the gyro calibration.
-    private void UpdateCalibration(bool onlyHorizontal) {
-        if (onlyHorizontal) {
-            var fw = (Input.gyro.attitude) * (-Vector3.forward);
-            fw.z = 0;
-            if (fw == Vector3.zero)
-                calibration = Quaternion.identity;
-            else
-                calibration = (Quaternion.FromToRotation(baseOrientationRotationFix * Vector3.up, fw));
-        }
-        else 
-            calibration = Input.gyro.attitude;
+                                              ConvertRotation(referanceRotation * Input.gyro.attitude),
+                                              lowPassFilter);
     }
 
     // Update the camera base rotation.
-    private void UpdateCameraBaseRotation(bool onlyHorizontal) {
-        if (onlyHorizontal) {
-            var fw = transform.forward;
-            fw.y = 0;
-            if (fw == Vector3.zero) 
-                cameraBase = Quaternion.identity;
-            else 
-                cameraBase = Quaternion.FromToRotation(Vector3.forward, fw);
-        }
+    private void UpdateBaseRotation() {
+        Vector3 forward = transform.forward;
+        forward.y = 0;
+        if (forward == Vector3.zero)
+            cameraBase = Quaternion.identity;
         else
-            cameraBase = transform.rotation;
+            cameraBase = Quaternion.FromToRotation(Vector3.forward, forward);
     }
 
+    // Recalculates reference rotation.
+    private void RecalculateReferenceRotation() {
+        referanceRotation = Quaternion.Inverse(baseOrientation) * calibration;
+    }
 
     // Converts the rotation from right handed to left handed.
     private static Quaternion ConvertRotation(Quaternion q) {
         return new Quaternion(q.x, q.y, -q.z, -q.w);
     }
 
-    // Gets the rot fix for different orientations.
-    private Quaternion GetRotFix() {
-        return Quaternion.identity;
-    }
 
     // Recalculates reference system.
     private void ResetBaseOrientation() {
-        baseOrientationRotationFix = GetRotFix();
-        baseOrientation = baseOrientationRotationFix * baseIdentity;
+        baseOrientation = Quaternion.identity * baseIdentity;
     }
-
-    // Recalculates reference rotation.
-    private void RecalculateReferenceRotation() {
-        referanceRotation = Quaternion.Inverse(baseOrientation) * Quaternion.Inverse(calibration);
-    }
-
 }
